2b83ae2f2586b13209eafe24dd12bf2111914de4
Anishghatage <53462792+Anishghatage@users.noreply.github.com>
2020-06-06 17:43:13 +0530

    Update VERSION.c

--- VERSION.c
+++ VERSION.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int fun(int,int);
+int fun(int,int,int*);
 int main()
-{
-    fun(3,6);
+{   int C=0;
+   
+    fun(3,6,&C);
+    printf("sum is %d\n",C);
 }
-int fun(int a,int b)
+int fun(int a,int b,int *PTR)
 {
-  printf("sum is = %d",a+b);
- return 0;
+  *PTR=a+b;
+   return 0;
 }
 
